3598c1e5f3fcc0f353f82626bec4c343a9f50da0
Dhanraj H Kutwade <110124614+dhanrajreddy@users.noreply.github.com>
2023-11-29 23:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
 function hasKey(key) {
-  //   write your code here
+  return Object.prototype.hasOwnProperty.call(sampleObject, key);
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// Sample object provided
+const sampleObject = {
+  red: "#FF0000",
+  green: "#00FF00",
+  white: "#FFFFFF"
+};
+
+// Test cases
+console.log(hasKey("red"));    // Output: true
+console.log(hasKey("blue"));   // Output: false
+console.log(hasKey("white"));  // Output: true
